2d284995740e7eea5e255d6a0270bacf3f1f0347
Ye-hor <48259081+Ye-hor@users.noreply.github.com>
2021-12-02 13:42:24 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,95 +1,143 @@
-const startBtn = document.querySelector('#start')
-const screens = document.querySelectorAll('.screen')
-const timeList = document.querySelector('#time-list')
-const timeEl = document.querySelector('#time')
-const board = document.querySelector('#board')
-const colors = ['#CD5C5C', '#F08080', '#FA8072', '#E9967A', '#FFA07A']
-const resetBtn = document.getElementById('reset-btn')
-let time = 0
-let score = 0
+const startBtn = document.querySelector('#start');
+const ratingBtn = document.querySelector('#rating');
+const startGameScreen = document.querySelector('.start-game');
+const timeSelectorScreen = document.querySelector('.time-selector');
+const gameScreen = document.querySelector('.game');
+const timeList = document.querySelector('#time-list');
+const timeEl = document.querySelector('#time');
+const board = document.querySelector('#board');
+const ulEl = document.getElementById('ul-el');
+const saveBtn = document.getElementById('save-btn');
+const resetBtn = document.getElementById('reset-btn');
+const colors = ['#CD5C5C', '#F08080', '#FA8072', '#E9967A', '#FFA07A'];
+let time = 0;
+let score = 0;
+let rating = [];
+let intervalId;
 
-startBtn.addEventListener('click', (event) => {
-    event.preventDefault()
-    screens[0].classList.add('up')
-})
+startBtn.addEventListener('click', event => {
+    event.preventDefault();
+    startGameScreen.classList.remove('visible');
+    timeSelectorScreen.classList.add('visible');
+});
+
+resetBtn.addEventListener('click', () => {
+    gameScreen.classList.remove('visible');
+    startGameScreen.classList.add('visible');
+    score = 0;
+    time = 0;
+});
 
 timeList.addEventListener('click', event => {
-    if (event.target.classList.contains('time-btn')) {
-        time = parseInt(event.target.getAttribute('data-time'))
-        screens[1].classList.add('up')
-        startGame()
-    }
-})
+    const buttonClickedElement = event.target;
+    const buttonClasses = buttonClickedElement.classList; // buttonClickedElement.getAttribute('class').split(' ')
+    const hasButtonClass = buttonClasses.contains('time-btn');
 
-board.addEventListener('click', event => {
-    if (event.target.classList.contains('circle')) {
-        score++
-        event.target.remove()
-        createRandomCircle()
+    if (hasButtonClass) {
+        time = parseInt(buttonClickedElement.getAttribute('data-time'));
+        timeSelectorScreen.classList.remove('visible');
+        gameScreen.classList.add('visible');
+        startGame();
     }
-})
+});
 
+function formatTime(rawTime) {
+    if (rawTime < 10) {
+        return `0${rawTime}`;
+    }
+
+    return rawTime;
+}
+
+// create TIME COUNTER - - - - -
 function startGame() {
-    setInterval(decreaseTime, 1000)
-    createRandomCircle()
-    setTime(time)
+    board.innerHTML = '';
+    saveBtn.disabled = true;
+    intervalId = setInterval(decreaseTime, 1000);
+    createRandomCircle();
+    setTime(time);
 }
 
 function decreaseTime() {
     if (time === 0) {
-        finishGame()
+        clearInterval(intervalId);
+        finishGame();
     } else {
-        let current = --time
-        if (current < 10) {
-            current = `0${current}`
-        }
-        setTime(current)
+        let current = --time;
+        console.log({ current, time });
+        setTime(current);
     }
 }
 
 function setTime(value) {
-    timeEl.innerHTML = `00:${value}`
+    timeEl.innerHTML = `00:${formatTime(value)}`;
 }
+// - - - - - - - - - - - - - - -
+
+board.addEventListener('click', event => {
+    if (event.target.classList.contains('circle')) {
+        score++;
+        event.target.remove();
+        createRandomCircle();
+    }
+});
 
 function finishGame() {
-    timeEl.parentNode.classList.add('hide')
-    board.innerHTML = `<h1> Cчет: <span class="primary">${score}</span></h1>`
-    resetBtn.addEventListener('click', () => {
-        location.reload()
-    })
+    // timeEl.parentNode.classList.add('hide')
+    board.innerHTML = `<h1> Cчет: <span class="primary">${score}</span></h1>`;
+    saveBtn.disabled = false;
+
+    saveBtn.addEventListener('click', () => {
+        rating.push(score);
+        localStorage.setItem('scoreInRating', JSON.stringify(rating));
+        render(rating);
+    });
 }
 
 function createRandomCircle() {
-    const circle = document.createElement('div')
-    const size = getRandomNumber(10, 60)
-    const {width, height} = board.getBoundingClientRect()
-    const x = getRandomNumber(0, width - size)
-    const y = getRandomNumber(0, height - size)
-    const color = getRandomColor()
+    const circle = document.createElement('div');
+    const size = getRandomNumber(20, 50);
+    const { width, height } = board.getBoundingClientRect();
+    const x = getRandomNumber(0, width - size);
+    const y = getRandomNumber(0, height - size);
+    const color = getRandomColor();
 
-    circle.classList.add('circle')
-    circle.style.width = `${size}px`
-    circle.style.height = `${size}px`
-    circle.style.top = `${y}px`
-    circle.style.left = `${x}px`
-    circle.style.backgroundColor = color
+    circle.classList.add('circle');
+    circle.style.width = `${size}px`;
+    circle.style.height = `${size}px`;
+    circle.style.top = `${y}px`;
+    circle.style.left = `${x}px`;
+    circle.style.backgroundColor = color;
 
-    board.append(circle)
+    board.append(circle);
 }
 
 function getRandomNumber(min, max) {
-    return Math.round(Math.random() * (max - min) + min)
+    return Math.round(Math.random() * (max - min) + min);
 }
 
 function getRandomColor() {
- const index = Math.floor(Math.random() * colors.length)
-  return colors[index]
+    return colors[Math.floor(Math.random() * colors.length)];
 }
+
+// function render(playerPoints) {
+//     let listItems = ""
+//     for (let i = 0; i < playerPoints.length; i++) {
+//         listItems += `
+//         <li>
+//         <a target='_blank' href='${playerPoints[i]}'>
+//         ${playerPoints[i]}
+//         </a>
+//         </li>
+//         `
+//     }
+//     ulEl.innerHTML = listItems
+// }
 
 // function winTheGame() {
 //     function kill() {
 //         const circle = document.querySelector('.circle')
-        
+
 //         if (circle) {
 //             circle.click()
 //         }
